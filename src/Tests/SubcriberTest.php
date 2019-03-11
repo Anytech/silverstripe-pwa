1c74a4b6bee9ca87f269fb229ea14f64d2feaac6
Michiel Diederen <mcf.diederen@gmail.com>
2019-03-11 15:05:44 +0100

    Added unittest for Subscriber-model

---
+++ src/Tests/SubcriberTest.php
@@ -0,0 +1,28 @@
+<?php
+
+use SilverStripe\Dev\SapphireTest;
+use SilverstripePWA\Models\Subscriber;
+
+class SubscriberTest extends SapphireTest
+{
+    public function testCreate()
+    {
+        $subscribers = Subscriber::get()->count();
+        $SubscriberInstance = Subscriber::Create();
+        $SubscriberObject = [
+                'endpoint' => 'Test123',
+                'publicKey' => 'Test123',
+                'authToken' => 'Test123',
+                'contentEncoding' => 'Test123'
+            ];
+
+            $SubscriberInstance->endpoint = $SubscriberObject['endpoint'];
+            $SubscriberInstance->publicKey = $SubscriberObject['publicKey'];
+            $SubscriberInstance->authToken = $SubscriberObject['authToken'];
+            $SubscriberInstance->contentEncoding = $SubscriberObject['contentEncoding'];
+
+            $SubscriberInstance->write();
+
+        $this->assertEquals($subscribers +1, Subscriber::get()->count());
+    }
+}
